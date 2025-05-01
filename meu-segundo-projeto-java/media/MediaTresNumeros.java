96ec7e91e580eb9a0bdc94899e58e137c4f54841
RondiDev <rondinele20@yahoo.com.br>
2025-05-01 01:26:58 -0300

    Media

---
+++ meu-segundo-projeto-java/media/MediaTresNumeros.java
@@ -0,0 +1,18 @@
+public class MediaTresNumeros {
+
+    public static void main(String[] args) {
+        // Defina os três números
+        double numero1 = 10.5;
+        double numero2 = 20.0;
+        double numero3 = 15.5;
+
+        // Calcule a soma dos três números
+        double soma = numero1 + numero2 + numero3;
+
+        // Calcule a média dividindo a soma por 3
+        double media = soma / 3;
+
+        // Imprima o resultado
+        System.out.println("A média dos números " + numero1 + ", " + numero2 + " e " + numero3 + " é: " + media);
+    }
+}
